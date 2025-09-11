3c927f16c1104dd50d183e8de3cd18c91c61b5b0
Md. Rakibul Hossain <134864638+rakibul6670@users.noreply.github.com>
2025-09-12 00:32:55 +0600

    Update factorial_number.dart

--- factorial_number.dart
+++ factorial_number.dart
@@ -1,1 +1,15 @@
 
+void main() {
+  
+  int input = 14;
+  
+  int factorial=1 ;
+  
+  for(var i = 1 ; i<= input; i++ ){
+   factorial *= i;
+   //print(i);
+  }
+  
+  
+  print("$input factorial = $factorial");
+}
