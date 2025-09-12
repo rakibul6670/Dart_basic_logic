8e11fda65cb93c9d625f2898bcbd9151f698441c
Rakibul <rakibulpb6670@gmail.com>
2025-09-12 12:32:20 +0600

    sentance

--- sentance_vowel_find.dart
+++ sentance_vowel_find.dart
@@ -1,3 +1,17 @@
 void main(){
-  String name = "Rakibul";
+  String name = "Rakibul aeiou";
+  String vowel ="aeiou";
+  int vowelCount =0;
+
+  List strings = name.split("");
+
+  for(var i in strings ){
+    if(vowel.contains(i)){
+      vowelCount++;
+
+    }
+  }
+
+
+  print("$name have $vowelCount vowel");
 }
